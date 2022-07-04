7ed56de7573ca95b7c7590578146897da0a74f42
kento0328 <usa.kento328@gmail.com>
2022-07-04 15:45:36 +0900

    add gendertousers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  enum gender:{
+    man: 0,
+    woman: 1
+  }
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :lockable, :confirmable, :timeoutable, :trackable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_060320) do
+ActiveRecord::Schema.define(version: 2022_07_04_063733) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2022_07_04_060320) do
     t.integer "icon"
     t.integer "child_age"
     t.integer "child_number"
+    t.integer "gender"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
