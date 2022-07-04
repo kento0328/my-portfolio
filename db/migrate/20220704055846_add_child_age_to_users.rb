22b8e0fac9c6fd73ddf9c401b80d4e2c4418426b
kento0328 <usa.kento328@gmail.com>
2022-07-04 15:00:29 +0900

    add child_agetousers

---
+++ db/migrate/20220704055846_add_child_age_to_users.rb
@@ -0,0 +1,5 @@
+class AddChildAgeToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :child_age, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_055153) do
+ActiveRecord::Schema.define(version: 2022_07_04_055846) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_07_04_055153) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "nickname"
     t.integer "icon"
+    t.integer "child_age"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
