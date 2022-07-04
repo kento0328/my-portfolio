ac8f2723a89afa89f1da606b99438b5c5ee9ffd8
kento0328 <usa.kento328@gmail.com>
2022-07-04 15:04:31 +0900

    add child_numbertousers

---
+++ db/migrate/20220704060320_add_child_number_to_users.rb
@@ -0,0 +1,5 @@
+class AddChildNumberToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :child_number, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_055846) do
+ActiveRecord::Schema.define(version: 2022_07_04_060320) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_07_04_055846) do
     t.string "nickname"
     t.integer "icon"
     t.integer "child_age"
+    t.integer "child_number"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
